00011ad14f9cc4a36bc2118ffafea7fc619c8599
anishashin <anishashin@gmail.com>
2022-04-06 00:04:49 -0400

    gift post/put/patch routes

--- backend/routes/gifts.js
+++ backend/routes/gifts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const xss = require("xss");
 const data = require("../data");
 const giftData = data.gifts;
 
@@ -39,6 +40,246 @@ router.get("/:giftId", async (req, res) => {
         return;
     }
     res.json(reqGift);
+});
+
+router.post("/", async (req, res) => {
+    let giftInfo = req.body;
+    giftInfo.title = xss(giftInfo.title);
+    giftInfo.price = xss(giftInfo.price);
+    giftInfo.url = xss(giftInfo.url);
+    giftInfo.picture = xss(giftInfo.picture);
+    giftInfo.description = xss(giftInfo.description);
+    if (!giftInfo) {
+        res.status(400).json({
+            message: "You must provide data to add a gift."
+        });
+        return;
+    }
+    giftInfo.parsedPrice = parseFloat(giftInfo.price);
+    if (!giftInfo.title || typeof giftInfo.title !== "string" || giftInfo.title.trim() === "") {
+        res.status(400).json({
+            message: "Invalid title."
+        });
+        return;
+    }
+    if (!giftInfo.parsedPrice || typeof giftInfo.parsedPrice !== "number" || giftInfo.parsedPrice < 0) {
+        res.status(400).json({
+            message: "Price must be a number greater than or equal to 0."
+        });
+        return;
+    }
+    if (!giftInfo.url || typeof giftInfo.url !== "string" || giftInfo.url.trim() === "") {
+        res.status(400).json({
+            message: "Invalid url."
+        });
+        return;
+    }
+    if (!giftInfo.picture || typeof giftInfo.picture !== "string" || giftInfo.picture.trim() === "") {
+        res.status(400).json({
+            message: "Invalid picture."
+        });
+        return;
+    }
+    if (!giftInfo.description || typeof giftInfo.description !== "string" || giftInfo.description.trim() === "") {
+        res.status(400).json({
+            message: "Invalid description."
+        });
+        return;
+    }
+    try {
+        const newGift = await giftData.create(
+            giftInfo.title,
+            giftInfo.parsedPrice,
+            giftInfo.url,
+            giftInfo.picture,
+            giftInfo.description
+        );
+        res.status(200).json(newGift);
+    } catch (e) {
+        res.status(500).json({
+            message: e.message
+        });
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    req.params.id = xss(req.params.id);
+    if (!req.params.id || typeof req.params.id !== "string" || req.params.id.trim() === "") {
+        res.status(400).json({
+            message: "Id must be a non-empty string containing more than just spaces."
+        });
+        return;
+    }
+    let giftInfo = req.body;
+    giftInfo.title = xss(giftInfo.title);
+    giftInfo.price = xss(giftInfo.price);
+    giftInfo.url = xss(giftInfo.url);
+    giftInfo.picture = xss(giftInfo.picture);
+    giftInfo.description = xss(giftInfo.description);
+    if (!giftInfo) {
+        res.status(400).json({
+            message: "You must provide data to edit a gift."
+        });
+        return;
+    }
+    giftInfo.id = req.params.id;
+    giftInfo.parsedPrice = parseFloat(giftInfo.price);
+    if (!giftInfo.title || typeof giftInfo.title !== "string" || giftInfo.title.trim() === "") {
+        res.status(400).json({
+            message: "Invalid title."
+        });
+        return;
+    }
+    if (!giftInfo.parsedPrice || typeof giftInfo.parsedPrice !== "number" || giftInfo.parsedPrice < 0) {
+        res.status(400).json({
+            message: "Price must be a number greater than or equal to 0."
+        });
+        return;
+    }
+    if (!giftInfo.url || typeof giftInfo.url !== "string" || giftInfo.url.trim() === "") {
+        res.status(400).json({
+            message: "Invalid url."
+        });
+        return;
+    }
+    if (!giftInfo.picture || typeof giftInfo.picture !== "string" || giftInfo.picture.trim() === "") {
+        res.status(400).json({
+            message: "Invalid picture."
+        });
+        return;
+    }
+    if (!giftInfo.description || typeof giftInfo.description !== "string" || giftInfo.description.trim() === "") {
+        res.status(400).json({
+            message: "Invalid description."
+        });
+        return;
+    }
+    try {
+        const gift = await giftData.get(req.params.id);
+    } catch (e) {
+        res.status(404).json({
+            message: "Gift not found."
+        });
+        return;
+    }
+    try {
+        const updatedGift = await giftData.update(
+            req.params.id,
+            giftInfo.title,
+            giftInfo.parsedPrice,
+            giftInfo.url,
+            giftInfo.picture,
+            giftInfo.description
+        );
+        res.status(200).json(updatedGift);
+    } catch (e) {
+        res.status(500).json({
+            message: e.message
+        });
+    }
+});
+
+router.patch("/:id", async (req, res) => {
+    req.params.id = xss(req.params.id);
+    if (!req.params.id || typeof req.params.id !== "string" || req.params.id.trim() === "") {
+        res.status(400).json({
+            message: "Id must be a non-empty string containing more than just spaces."
+        });
+        return;
+    }
+    let gift;
+    try {
+        gift = await giftData.get(req.params.id);
+    } catch (e) {
+        res.status(404).json({
+            message: "Gift not found."
+        });
+        return;
+    }
+    let giftInfo = req.body;
+    if (!giftInfo) {
+         res.status(400).json({
+            message: "You must provide data to edit a gift."
+        });
+        return;
+    }
+    giftInfo.id = req.params.id;
+    if (giftInfo.hasOwnProperty("title")) {
+        giftInfo.title = xss(giftInfo.title);
+        if (!giftInfo.title || typeof giftInfo.title !== "string" || giftInfo.title.trim() === "") {
+            res.status(400).json({
+                message: "Invalid title."
+            });
+            return;
+        }
+    }
+    else {
+        giftInfo.title = gift.title;
+    }
+    if (giftInfo.hasOwnProperty("price")) {
+        giftInfo.price = xss(giftInfo.price);
+        giftInfo.parsedPrice = parseFloat(giftInfo.price);
+        if (!giftInfo.parsedPrice || typeof giftInfo.parsedPrice !== "number" || giftInfo.parsedPrice < 0) {
+            res.status(400).json({
+                message: "Price must be a number greater than or equal to 0."
+            });
+            return;
+        }
+    }
+    else {
+        giftInfo.parsedPrice = gift.price;
+    }
+    if (giftInfo.hasOwnProperty("url")) {
+        giftInfo.url = xss(giftInfo.url);
+        if (!giftInfo.url || typeof giftInfo.url !== "string" || giftInfo.url.trim() === "") {
+            res.status(400).json({
+                message: "Invalid url."
+            });
+            return;
+        }
+    }
+    else {
+        giftInfo.url = gift.url;
+    }
+    if (giftInfo.hasOwnProperty("picture")) {
+        giftInfo.picture = xss(giftInfo.picture);
+        if (!giftInfo.picture || typeof giftInfo.picture !== "string" || giftInfo.picture.trim() === "") {
+            res.status(400).json({
+                message: "Invalid picture."
+            });
+            return;
+        }
+    }
+    else {
+        giftInfo.picture = gift.picture;
+    }
+    if (giftInfo.hasOwnProperty("description")) {
+        giftInfo.description = xss(giftInfo.description);
+        if (!giftInfo.description || typeof giftInfo.description !== "string" || giftInfo.description.trim() === "") {
+            res.status(400).json({
+                message: "Invalid description."
+            });
+            return;
+        }
+    }
+    else {
+        giftInfo.description = gift.description;
+    }
+    try {
+        const updatedGift = await giftData.update(
+            req.params.id,
+            giftInfo.title,
+            giftInfo.parsedPrice,
+            giftInfo.url,
+            giftInfo.picture,
+            giftInfo.description
+        );
+        res.status(200).json(updatedGift);
+    } catch (e) {
+        res.status(500).json({
+            message: e.message
+        });
+    }
 });
 
 // DELETE localhost:3001/gifts/:giftId
