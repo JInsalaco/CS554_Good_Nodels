838399e583db5853695d219764a897994561e470
anishashin <anishashin@gmail.com>
2022-04-17 00:48:49 -0400

    start of individual gift page

--- frontend/src/components/GiftPage.js
+++ frontend/src/components/GiftPage.js
@@ -1,13 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function GiftPage() {
+    const [giftData, setGiftData] = useState(undefined);
     let { id } = useParams();
     
     useEffect(() => {
+        setGiftData(undefined);
+        async function fetchData() {
+            try {
+                const { data } = await axios.get(`http://localhost:3001/gifts/${id}`);
+                setGiftData(data);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        fetchData();
     }, [id]);
 
     return (
@@ -15,7 +27,7 @@ function GiftPage() {
             <br />
             <br />
             <Container className='container'>
-                <div>{id}</div>
+                <div>{JSON.stringify(giftData)}</div>
             </Container>
         </div>
     );
